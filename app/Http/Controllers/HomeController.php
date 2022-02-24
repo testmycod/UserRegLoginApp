9e6490399cef0412f18e4fe85f2d5e9775e9b321
Dilip <dilip05292019@gmail.com>
2022-02-24 06:02:51 -0500

    SAPSOL Project
    User Login details

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $ver = array('verified','verified');
+        $this->middleware('auth',$ver);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
